abe6acca0b498c0594eb78825c208bbcb3329b8d
MoriatyK95 <49929008+MoriatyK95@users.noreply.github.com>
2020-07-24 13:41:30 -0700

    working on question 3
    learning about neural network at the moment to attempt on question 3

---
+++ programming_interview_v10/Question 7/Question 7.1/indexToCoordinates.py
@@ -0,0 +1,20 @@
+import csv
+#Author: Huakai
+#Date: 25/07/2020
+#Description: Q7 ASTAR interview map 1D to 2D
+
+
+outputfile = open("output_coordinates_7_1.txt", "w")
+
+
+with open("input_index_7_1.txt") as f:
+    next(f)
+    for line in f:
+        c = line
+        c = int(c)
+        x1 = c % 50
+        #floor of c divide by n + 1 to get x2
+        x2 = c //  (57 + 1)
+        print(x1 , x2)
+        outputfile.write(str(x1)  + str(x2) + "\n")
+outputfile.close()1
