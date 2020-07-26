0a648e5eb102feb77f1ea3900577ad6350fbdefe
MoriatyK95 <49929008+MoriatyK95@users.noreply.github.com>
2020-07-25 17:29:33 -0700

    given test data

--- programming_interview_v10/Question 3/Perceptron.py
+++ programming_interview_v10/Question 3/Perceptron.py
@@ -8,11 +8,15 @@ import pandas as pd
 import tensorflow as tf
 
 if __name__ == '__main__':
+    #get training data using pandas
     train_data = pd.read_table("train_data.txt")
+    #turn
     train_df = pd.DataFrame(train_data)
     truth_data = pd.read_table("train_truth.txt")
     train_truth_df = pd.DataFrame(truth_data)
     # print(truth_data.shape)
+
+    #merge train_df and train_truth_df
     data = pd.merge(train_df, train_truth_df, left_index=True, right_index=True)
 
     X = data[['x1', 'x2', 'x3']].to_numpy()
@@ -30,12 +34,16 @@ if __name__ == '__main__':
     model.add(tf.keras.layers.Dense(4, activation=tf.nn.relu))
 
     #1 output node
-    model.add(tf.keras.layers.Dense(1, activation=tf.nn.elu))
+    model.add(tf.keras.layers.Dense(1, activation=tf.nn.relu))
 
-    model.compile(optimizer= 'adam', loss='mean_squared_error', metrics=[tf.keras.metrics.Accuracy()])
-    model.fit(X, Y, epochs= 100)
+    model.compile(optimizer= 'adam', loss='mean_squared_error', metrics=[tf.keras.metrics.MeanSquaredError()])
+    model.fit(X, Y, epochs= 600)
 
+    #get testing_data
+    test_data = pd.read_table("test_data.txt")
+    test_df = pd.DataFrame(train_data)
+    X_test = test_df[['x1', 'x2', 'x3']].to_numpy()
+    X_test = np.array(X_test)
 
-
-    prediction = model.predict(X)
-    print(prediction)
+    prediction = model.predict(X_test)
+    #print(prediction)
